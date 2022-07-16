b1a365ce4a730b8eb8cc665809d1e6b94d3dc15e
Hamzah Haseeb <105497224+Hamzah840@users.noreply.github.com>
2022-07-16 20:51:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+// include 'compress_img.php';
+require 'header.php';
+
+?>
+<meta name="discription" content="Best Online Tools free">
+<meta name="keywords" content="online tools, best online tools, web tools, All kind of online tools, online tools free, best online tools free">
+<title>Home - <?php echo $title ?></title>
+
+<div class="home-container">
+    
+    <section class="online-tools">
+        <div class="tools-container flex">
+            
+            <a href="./compressjpeg.php">
+            <div class="tool-box flex">
+                <i class="fa fa-compress"></i>
+                <span class="tool-title">Image Compressor</span>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="tool-box flex">
+                <i class="fa fa-file-image-o"></i>
+                <span class="tool-title">Coming soon</span>
+            </div>
+            </a>
+            
+            <a href="#">
+            <div class="tool-box flex">
+                <i class="fa fa-file-pdf-o"></i>
+                <span class="tool-title">Coming soon</span>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="tool-box flex">
+                <i class="fa fa-file-video-o"></i>
+                <span class="tool-title">Coming soon</span>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="tool-box flex">
+                <i class="fa fa-file-code-o"></i>
+                <span class="tool-title">Coming soon</span>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="tool-box flex">
+                <i class="fa fa-file-archive-o"></i>
+                <span class="tool-title">Coming soon</span>
+            </div>
+            </a>
+
+        </div>
+    </section>
+
+</div>
+
+<?php require 'footer.php' ?>
